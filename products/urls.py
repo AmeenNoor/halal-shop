23587afa23bab248f86f1824f86a7f62001c3409
Ameen Noor <ameennoor2007@hotmail.com>
2024-04-30 18:28:53 +0000

    Set up URL routes for the product removal functionality

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,7 @@ from .views import (
     ProductDetail,
     ProductCreate,
     ProductUpdate,
+    ProductDelete,
 )
 
 urlpatterns = [
@@ -11,4 +12,5 @@ urlpatterns = [
     path('<int:pk>/', ProductDetail.as_view(), name='product_detail'),
     path('create/', ProductCreate.as_view(), name='product_create'),
     path('<int:pk>/edit/', ProductUpdate.as_view(), name='product_update'),
+    path('<int:pk>/delete/', ProductDelete.as_view(), name='product_delete'),
 ]
