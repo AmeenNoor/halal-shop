a8450e636d371c70dab13f66c190a8b2c26e310c
Ameen Noor <ameennoor2007@hotmail.com>
2024-04-30 17:07:08 +0000

    Set up URL routes for the product editing functionality

--- products/urls.py
+++ products/urls.py
@@ -3,10 +3,12 @@ from .views import (
     ProductList,
     ProductDetail,
     ProductCreate,
+    ProductUpdate,
 )
 
 urlpatterns = [
     path('', ProductList.as_view(), name='products'),
     path('<int:pk>/', ProductDetail.as_view(), name='product_detail'),
     path('create/', ProductCreate.as_view(), name='product_create'),
+    path('<int:pk>/edit/', ProductUpdate.as_view(), name='product_update'),
 ]
