1f73da83425f710825a6e1090f7bc7690309be41
Ameen Noor <ameennoor2007@hotmail.com>
2024-04-30 03:45:45 +0000

    Set up URL routes for the product addition functionality

--- products/urls.py
+++ products/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import (
     ProductList,
     ProductDetail,
+    ProductCreate,
 )
 
 urlpatterns = [
     path('', ProductList.as_view(), name='products'),
     path('<int:pk>/', ProductDetail.as_view(), name='product_detail'),
+    path('create/', ProductCreate.as_view(), name='product_create'),
 ]
