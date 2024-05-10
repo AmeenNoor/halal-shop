f1cc90f6d7f502a75ff40d49fc9d2651864b77d2
Ameen Noor <ameennoor2007@hotmail.com>
2024-05-10 03:08:42 +0000

    Add functionality to restrict access to checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class CheckoutView(TemplateView):
+class CheckoutView(LoginRequiredMixin,TemplateView):
     template_name = 'checkout.html'
