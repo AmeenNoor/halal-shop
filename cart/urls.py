33b8ae915ae4644f0a611ddd58df3a48a0eb3a37
Ameen Noor <ameennoor2007@hotmail.com>
2024-05-10 22:30:03 +0000

    Set up URL routes for handling remove items from cart

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CartAddView, CartListView
+from .views import CartAddView, CartListView, CartDeleteView
 
 urlpatterns = [
     path('add/', CartAddView.as_view(), name='cart_add'),
     path('', CartListView.as_view(), name='cart'),
+    path('delete/', CartDeleteView.as_view(), name='cart_delete'),
 ]
