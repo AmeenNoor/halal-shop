227eed54c690859acb32f3afc0fef6103c4e29d3
Ameen Noor <ameennoor2007@hotmail.com>
2024-05-10 05:29:55 +0000

    Add functionality to display the checkout form with fields

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import CheckoutForm
+from cart.contexts import cart_total
 
-class CheckoutView(LoginRequiredMixin,TemplateView):
+class CheckoutView(LoginRequiredMixin,FormView):
+    form_class = CheckoutForm
     template_name = 'checkout.html'
+    success_url = '/checkout/success/' 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cart_items'] = self.request.session.get('cart', {})
+        context.update(cart_total(self.request))
+        return context
+
+    def form_valid(self, form):
+        return super().form_valid(form)
