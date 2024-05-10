e267632205021e8394f6695a8779eb791015ede1
Ameen Noor <ameennoor2007@hotmail.com>
2024-05-10 04:32:11 +0000

    Add basic styling for checkout page

--- checkout/models.py
+++ checkout/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from products.models import Product
 import uuid
+from django.db.models import Sum
+from django.conf import settings
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
