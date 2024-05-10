f47c8b2cd596f1693c407c359db94cdb9ae95698
Ameen Noor <ameennoor2007@hotmail.com>
2024-05-10 03:47:23 +0000

    Add OrderItem model for checkout items

--- checkout/models.py
+++ checkout/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from products.models import Product
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -19,3 +20,11 @@ class Order(models.Model):
 
     def __str__(self):
         return self.order_number
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} ({self.order.order_number})"
