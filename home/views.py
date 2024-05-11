b67f67163b83529e5f464e15be2be2ceae51cae2
Ameen Noor <ameennoor2007@hotmail.com>
2024-05-11 21:43:35 +0000

    Add HomeView for the home page and corresponding URL configuration

--- home/views.py
+++ home/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from products.models import Product
 
-# Create your views here.
+class HomeView(TemplateView):
+    """
+    View for home page
+    """
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_products'] = Product.objects.order_by('-id')[:3]
+        return context
