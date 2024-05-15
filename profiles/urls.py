61a8636ca20a19a74e5cb82199a8067738651ee5
Ameen Noor <ameennoor2007@hotmail.com>
2024-05-15 02:28:34 +0000

    Add URL for order history and create order history template

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AccountView
+from .views import AccountView, OrderHistoryView
 
 urlpatterns = [
     path('', AccountView.as_view(), name='profile'),
+    path('order_history/', OrderHistoryView.as_view(), name='order_history'),
 ]
