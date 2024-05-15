0eca3074f8dbac443c7fd9d2c9f68daab7bea71e
Ameen Noor <ameennoor2007@hotmail.com>
2024-05-15 00:43:15 +0000

    Add AccountView for user profile display

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, LoginRequiredMixin
+from .models import UserProfile
 
-# Create your views here.
+class AccountView(LoginRequiredMixin, TemplateView):
+    model = UserProfile
+    template_name = 'profile.html'
