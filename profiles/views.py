f3e3ac138f1eb58e70fcf8b3e47bfc534db83341
Ameen Noor <ameennoor2007@hotmail.com>
2024-05-15 01:01:05 +0000

    Add profile URL and template

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, LoginRequiredMixin
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import UserProfile
 
 class AccountView(LoginRequiredMixin, TemplateView):
