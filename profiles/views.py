ae875f2da8633386051c58ae33d60c0feedbbfe0
Ameen Noor <ameennoor2007@hotmail.com>
2024-05-15 02:09:20 +0000

    Add OrderHistoryView for displaying user order history

--- profiles/views.py
+++ profiles/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import UserProfile
+from checkout.models import Order
 
 class AccountView(LoginRequiredMixin, TemplateView):
     model = UserProfile
     template_name = 'profile.html'
+
+class OrderHistoryView(LoginRequiredMixin, TemplateView):
+    template_name = 'order_history.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        orders = Order.objects.filter(user=self.request.user)
+        context['orders'] = orders
+        return context
