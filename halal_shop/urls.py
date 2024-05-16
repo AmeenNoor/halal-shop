bd2dcfa6d80ef8cd7bd59c69ddbff0d2d8f5d570
Ameen Noor <ameennoor2007@hotmail.com>
2024-05-16 04:15:30 +0000

    Add custom 500 error handling

--- halal_shop/urls.py
+++ halal_shop/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from .views import handler404
+from .views import handler404, handler500
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -30,3 +30,4 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'halal_shop.views.handler404'
+handler500 = 'halal_shop.views.handler500'
